e5de92cbf3b2de8c84ed8353582a807cfe5eec59
Alex Marandon <al@alpage.org>
2008-06-22 18:18:50 +0100

    Improved the look of pages.

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ DATE_FORMAT = '%d/%m/%Y'
 class Event(db.Model):
     date = db.DateTimeProperty(required=True)
     location = db.StringProperty(required=True, verbose_name='Lieu')
-    title = db.StringProperty(verbose_name='Intitulé (optionnel)')
-    description = db.TextProperty()
+    title = db.StringProperty(default=" ", verbose_name='Intitulé (optionnel)')
+    description = db.TextProperty(default=" ")
 
     @property
     def formatted_date(self):
@@ -39,6 +39,12 @@ class Event(db.Model):
 class EventForm(djangoforms.ModelForm):
     class Meta:
         model = Event
+
+
+class AdminPage(webapp.RequestHandler):
+
+    def get(self):
+        self.redirect("/admin/events")
 
 
 class EventsDeleteHandler(webapp.RequestHandler):
@@ -86,8 +92,9 @@ class EventsManager(webapp.RequestHandler):
         else:
             template_params = dict(events=Event.get_reversed_list(),
                     form=data, operation=operation)
-            self.response.out.write(template.render('templates/events_manager.html',
-                template_params))
+            self.response.out.write(
+                    template.render('templates/events_manager.html',
+                        template_params))
 
 
 class IndexPage(webapp.RequestHandler):
@@ -111,6 +118,7 @@ def main():
     logging.getLogger().setLevel(logging.DEBUG)
     application = webapp.WSGIApplication( [ 
                 ('/', IndexPage), 
+                ('/admin', AdminPage),
                 ('/admin/events', EventsManager),
                 ('/admin/events/delete', EventsDeleteHandler),
                 ('/tests', TestPage),
