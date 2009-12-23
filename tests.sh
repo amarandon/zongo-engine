288f35165aaf5ac74862726cefcf0ae317640466
Alex Marandon <alex.marandon@gmail.com>
2009-12-23 22:59:40 +0900

    Changed backgroung color.

---
+++ tests.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+PYTHONPATH=../../google_appengine:../../python_modules/nose-0.11.1-py2.6.egg:../../python_modules/nose-0.11.1-py2.6.egg:../../python_modules/NoseGAE-0.1.4-py2.6.egg:../../python_modules/WebTest-1.2-py2.6.egg python2.5  ../../python_modules/nosetests
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,5 @@
 from . import BaseTest
-from main import Link
+from main import Link, Event
 
 class TestModels(BaseTest):
 
@@ -8,4 +8,9 @@ class TestModels(BaseTest):
         link = Link(url=url)
         link.put()
         self.assertEqual(Link.gql("WHERE url = :1", url).count(), 1)
+
+    def test_main_image(self):
+        event = Event(title='foo', location='bar', date='2009-01-01')
+        event.put()
+
 
