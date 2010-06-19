c897ff2a98e9cfdfce121281d66702efafd34bb1
Alex Marandon <contact@alexmarandon.com>
2010-06-19 19:23:20 +0700

    Experimenting with paver tasks

--- pavement.py
+++ pavement.py
@@ -1,13 +1,30 @@
+import os, sys
+
 from paver.easy import *
 import paver.doctools
 from paver.setuputils import setup
 
+gaepath = os.environ.get("GAEPATH")
+if not gaepath:
+    sys.abort("Please set the GAEPATH environment variable to the path of the Google App Engine SDK.")
+sys.path.append(os.path.join(gaepath, 'lib', 'yaml', 'lib'))
+sys.path.append(os.path.join(gaepath, 'lib', 'antlr3'))
+sys.path.append(os.path.join(gaepath, 'lib', 'ipaddr'))
+
+from google.appengine.tools import appcfg
+
+@task
+def startserver():
+    sh("%s/dev_appserver.py ." % gaepath)
+
 @task
 def dumpdb():
     print "Downloading data from App Engine"
-    sh("python2.5 ../../google_appengine/bulkloader.py --dump --app_id=zongo --email=alex.marandon@gmail.com --url=http://www.zongosound.com/remote_api --filename=data.csv")
+    sh("%s/bulkloader.py --dump --app_id=zongo --email=alex.marandon@gmail.com --url=http://www.zongosound.com/remote_api --filename=data.csv" % gaepath)
 
 @task
 def restoredb():
     print "Loading data to development datastore."
-    sh("python2.5 ../../google_appengine/bulkloader.py --app_id=zongo --batch_size=1 --restore --url=http://localhost:8080/remote_api --email=admin --filename=data.csv")
+    args = "appcfg.py upload_data -A zongo --batch_size=1 --url=http://localhost:8080/remote_api --email=admin --filename=data.csv"
+    argv = args.split()
+    appcfg.main(argv)
