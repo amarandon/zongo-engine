3b568b0343e1cbf9256caa181c672449faf01ddc
Alex Marandon <contact@alexmarandon.com>
2012-04-26 10:26:16 +0200

    Adjust path to GAE DSK

--- pavement.py
+++ pavement.py
@@ -5,9 +5,9 @@ from paver.setuputils import setup
 @task
 def dumpdb():
     print "Downloading data from App Engine"
-    sh("python2.5 ../../google_appengine/bulkloader.py --dump --app_id=zongo --email=alex.marandon@gmail.com --url=http://www.zongosound.com/remote_api --filename=data.csv")
+    sh("python2.5 ../../lib/google_appengine/bulkloader.py --dump --app_id=zongo --email=alex.marandon@gmail.com --url=http://www.zongosound.com/remote_api --filename=data.csv")
 
 @task
 def restoredb():
     print "Loading data to development datastore."
-    sh("python2.5 ../../google_appengine/bulkloader.py --app_id=zongo --batch_size=1 --restore --url=http://localhost:8080/remote_api --email=admin --filename=data.csv")
+    sh("python2.5 ../../lib/google_appengine/bulkloader.py --app_id=zongo --batch_size=1 --restore --url=http://localhost:8080/remote_api --email=admin --filename=data.csv")
